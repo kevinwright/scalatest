d2b6285d1598e70751a82ca0210967a532dbf460
bv <bv@f1e4913d-8d42-0410-a976-cff77615562f>
2010-08-28 01:50:29 +0000

    dropped unneeded imports
    git-svn-id: https://scalatest.dev.java.net/svn/scalatest/branches/app/trunk-for-scala-2.8h@2252 f1e4913d-8d42-0410-a976-cff77615562f

--- src/main/scala/org/scalatest/TruncatedStackTraces.scala
+++ src/main/scala/org/scalatest/TruncatedStackTraces.scala
@@ -1,9 +1,5 @@
 package org.scalatest
 
-import org.scalatest.Suite
-import org.scalatest.AbstractSuite
-import org.scalatest.StackDepthException
-
 trait TruncatedStackTraces extends AbstractSuite { this: Suite =>
 
   abstract override def withFixture(test: NoArgTest) {
